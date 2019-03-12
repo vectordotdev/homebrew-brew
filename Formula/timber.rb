fa1ec43394b68bc734349968d52e384833f7eb14
Ben Johnson <bjohnson@binarylogic.com>
2019-03-12 16:43:50 -0400

    Initial commit

---
+++ Formula/timber.rb
@@ -0,0 +1,38 @@
+class Timber < Formula
+  desc "Timber.io CLI - Live tail and search your logs"
+  homepage "https://cli.heroku.com"
+  url "https://cli-assets.heroku.com/heroku-v7.22.4/heroku-v7.22.4.tar.xz"
+  sha256 "17d7e7be6286c1c09b80b95b48992321717efd725a7aa3f9587ffb63dbdcf318"
+  depends_on "heroku/brew/heroku-node"
+
+  def install
+    inreplace "bin/heroku", /^CLIENT_HOME=/, "export HEROKU_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/heroku", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/heroku"
+
+    bash_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/bash"
+    zsh_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/zsh/_heroku"
+  end
+
+  def caveats; <<~EOS
+    To use the Heroku CLI's autocomplete --
+      Via homebrew's shell completion:
+        1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
+            NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
+                  and called, either explicitly or via a framework like oh-my-zsh.
+        2) Then run
+          $ heroku autocomplete --refresh-cache
+
+      OR
+
+      Use our standalone setup:
+        1) Run and follow the install steps:
+          $ heroku autocomplete
+  EOS
+  end
+
+  test do
+    system bin/"heroku", "version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Timber Homebrew Tap
+
+Install the Timber CLI with
+
+    $ brew tap timberio/brew && brew install timber
+
+For more information, visit https://github.com/timberio/cli
