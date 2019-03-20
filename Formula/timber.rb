643528273a91f4efab226a7b7de346c2ef7fcacc
Ben Johnson <bjohnson@binarylogic.com>
2019-03-20 16:55:04 -0400

    timber v0.2.0

--- Formula/timber.rb
+++ Formula/timber.rb
@@ -1,8 +1,8 @@
 class Timber < Formula
   desc "Timber.io CLI - Live tail and search your logs"
   homepage "https://github.com/timberio/cli"
-  url "https://packages.timber.io/cli/0.1.0/amd64/timber-0.1.0-darwin-amd64.tar.gz"
-  sha256 "cdcea433106f10190146bb0a7ae651a51057a4a3fc0fd51b55110777080cd34e"
+  url "https://packages.timber.io/cli/0.2.0/amd64/timber-0.2.0-darwin-amd64.tar.gz"
+  sha256 "24f3258cf2978f6120240cea3049dd34bbbdc45b1664f4ed44d1f5379e9e8ed5"
   
   def install
     bin.install "bin/timber"
