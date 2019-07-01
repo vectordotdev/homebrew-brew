dc013d4470cd366606cd4eac53300001548a5de8
Ben Johnson <bjohnson@binarylogic.com>
2019-07-01 00:35:41 -0400

    Update

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -1,20 +1,26 @@
 class Vector < Formula
   desc "A High-Performance Log, Metrics, and Events Router"
   homepage "https://github.com/timberio/vector"
-  url "https://packages.timber.io/vector/vector-v0.2.0-dev.1-7-gbbf04c9-x86_64-unknown-linux-gnu.tar.gz"
-  sha256 "d121823513109d9664ca816c5362268015c916fdd66a732101902c174c28e223"
+  url "https://packages.timber.io/vector/v0.2.0-109-g6016e92/vector-v0.2.0-109-g6016e92-x86_64-apple-darwin.tar.gz"
+  sha256 "31bd9a4fa4cbfa888c80fa4e0c0cf47246f0e641baf9b15faae1802d105832e7"
+  head "https://github.com/timberio/vector.git"
   
   def install
-    bin.install "vector"
+    bin.install "bin/vector"
 
     # Set up Vector for local development
-    inreplace "#{libexec}/config/vector.toml" do |s|
-      s.gsub!(/data_dir: ".*"/, "data_dir: #{var}/lib/vector/")
+    inreplace "config/vector.toml" do |s|
+      s.gsub!(/data_dir = ".*"/, "data_dir: #{var}/lib/vector/")
     end
 
     # Move config files into etc
-    (etc/"vector").install Dir[libexec/"config/*"]
-    (libexec/"config").rmtree
+    (etc/"vector").install Dir["config/*"]
+    
+    begin
+      FileUtils.rm_rf("config")
+    rescue
+      # Swallow errors if this causes any
+    end
   end
 
   def post_install
@@ -68,7 +74,8 @@ class Vector < Formula
   end
 
   test do
-    output = shell_output("#{bin}/vector --version").chomp
-    assert_equal "Vector v#{version}", output
+    # output = shell_output("#{bin}/vector --version").chomp
+    # assert_equal "vector", output
+    system "false"
   end
 end
