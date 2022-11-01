69b9087d5275b9e6032b25fd6e6d62eed2d22dbf
vic <vector@datadoghq.com>
2022-11-01 16:17:42 +0000

    Release Vector 0.25.0

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -1,9 +1,9 @@
 class Vector < Formula
   desc "A High-Performance Log, Metrics, and Events Router"
   homepage "https://github.com/timberio/vector"
-  url "https://packages.timber.io/vector/0.24.2/vector-0.24.2-x86_64-apple-darwin.tar.gz"
-  version "0.24.2"
-  sha256 "c4f773a3d3129dc7daafedc337d7c90c33798261ac2dbce871adb4584d6abaa7"
+  url "https://packages.timber.io/vector/0.25.0/vector-0.25.0-x86_64-apple-darwin.tar.gz"
+  version "0.25.0"
+  sha256 "a1e417a4dee336438a3e427d2aaa7ee5412b80b2171b511dd9723bce1c6583a0"
   head "https://github.com/timberio/vector.git"
 
   def install
