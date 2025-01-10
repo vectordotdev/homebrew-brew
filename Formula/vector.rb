2776c74a2f9682036c2d424ea650019de073cba7
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-01-10 10:26:43 -0500

    feat(build): support Apple silicon (ARM)

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -1,9 +1,20 @@
 class Vector < Formula
   desc "A High-Performance Log, Metrics, and Events Router"
   homepage "https://github.com/timberio/vector"
-  url "https://packages.timber.io/vector/0.43.1/vector-0.43.1-x86_64-apple-darwin.tar.gz"
   version "0.43.1"
-  sha256 "c5e742e3bf55a6eca78c00ac585d3e4a3ea2cfd7890855f1e5f7e2b6ca84d1c0"
+
+  on_macos do
+    on_intel do
+      url "https://packages.timber.io/vector/0.43.1/vector-0.43.1-x86_64-apple-darwin.tar.gz"
+      sha256 "x86_64_SHA256_HASH_PLACEHOLDER" # x86_64-apple-darwin
+    end
+
+    on_arm do
+      url "https://packages.timber.io/vector/0.43.1/vector-0.43.1-arm64-apple-darwin.tar.gz"
+      sha256 "arm64_SHA256_HASH_PLACEHOLDER" # arm64-apple-darwin
+    end
+  end
+
   head "https://github.com/timberio/vector.git"
 
   def install
