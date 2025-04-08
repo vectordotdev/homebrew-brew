c8eea39d8392beb0258b41356cf5ae263f016138
vic <vector@datadoghq.com>
2025-04-08 13:58:13 -0400

    Release Vector 0.46.0

--- Formula/vector.rb
+++ Formula/vector.rb
@@ -1,17 +1,17 @@
 class Vector < Formula
   desc "A High-Performance Log, Metrics, and Events Router"
   homepage "https://github.com/timberio/vector"
-  version "0.45.0"
+  version "0.46.0"
 
   on_macos do
     on_intel do
-      url "https://packages.timber.io/vector/0.45.0/vector-0.45.0-x86_64-apple-darwin.tar.gz" # x86_64 url
-      sha256 "cfd5f9cb8bcb8a73a87a3e8c404e887ec5a876a8adc6a4e46ca849b3db151d19" # x86_64 sha256
+      url "https://packages.timber.io/vector/0.46.0/vector-0.46.0-x86_64-apple-darwin.tar.gz" # x86_64 url
+      sha256 "eb4bd47bd11fcd3d05dd1691cccd53e8eac17d97c8fb823e79a9f1c10cd54ac2" # x86_64 sha256
     end
 
     on_arm do
-      url "https://packages.timber.io/vector/0.45.0/vector-0.45.0-arm64-apple-darwin.tar.gz" # arm64 url
-      sha256 "5dd47b634497db9e358f1122918b492ef9e0cfe1d0f144d66a52118571145644" # arm64 sha256
+      url "https://packages.timber.io/vector/0.46.0/vector-0.46.0-arm64-apple-darwin.tar.gz" # arm64 url
+      sha256 "685e4c4d75e38a5421ee20604d2fca28b5c163200203059e707ae16ab6e52e68" # arm64 sha256
     end
   end
 
